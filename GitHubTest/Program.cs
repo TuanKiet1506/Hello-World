4902711ef94193edce0f946f23b64cfa54d8460a
Kiet Le <kietl6955@gmail.com>
2026-03-19 01:10:54 +0700

    Initial commit

---
+++ GitHubTest/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void WriteSomeThing(string sth)
+    {
+        Console.WriteLine($"{sth}"); 
+    }
+    public static void Main(String[] args)
+    {
+        WriteSomeThing("Hello VietNam"); 
+    }
+}
